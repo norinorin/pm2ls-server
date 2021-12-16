cc7aec0b42026baf94b1dd24e2d6c6c962ca3a60
Norizon <69400842+norinorin@users.noreply.github.com>
2021-12-16 14:08:20 +0700

    Fix some things

--- src/index.mjs
+++ src/index.mjs
@@ -3,17 +3,32 @@ import opus from '@discordjs/opus';
 import Speaker from 'speaker';
 
 const WS_PORT = 7619;
-let encoder = new opus.OpusEncoder(48000, 1);
-let speaker = new Speaker({ channels: 1, bitDepth: 16, sampleRate: 48000 });
+let inUse = false;
 let wss = new WebSocketServer({ port: WS_PORT, host: "0.0.0.0" });
 
 wss.on('connection', (ws, req) => {
     console.log("Got a connection...", req.headers["sec-websocket-key"]);
+    if (inUse) {
+        console.log("Another connection already exists! Aborting...");
+        return;
+    }
+    inUse = true;
+    let speaker;
+    let encoder;
     ws.on('message', (message) => {
-        const decoded = encoder.decode(message);
-        speaker.write(decoded);
+        // first message is the sample rate information
+        if (!speaker) /* && !encoder */ {
+            let sampleRate = parseInt(message.toString());
+            console.log("Sample rate: ", sampleRate);
+            speaker = new Speaker({ channels: 1, bitDepth: 16, sampleRate: sampleRate });
+            encoder = new opus.OpusEncoder(sampleRate, 1);
+            return;
+        }
+
+        speaker.write(encoder.decode(message));
     });
-    ws.on('close', _ => (
-        console.log(req.headers["sec-websocket-key"], "has disconnected")
-    ));
+    ws.on('close', _ => {
+        console.log(req.headers["sec-websocket-key"], "has disconnected");
+        inUse = false;
+    });
 });
